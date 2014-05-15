f009f88ec4efe5005215dca02b4cf25fa207f96f
yuripetnys <yuri.oliveira@gmail.com>
2014-05-15 10:12:01 -0300

    Initial commit

---
+++ js/pomodoro.js
@@ -0,0 +1,254 @@
+AudioManager = {}
+AudioManager.AudioLoaded = false;
+AudioManager.AudioFile = new Audio();
+AudioManager.AudioFile.addEventListener('canplaythrough', function () { AudioManager.AudioLoaded = true; }, false);
+AudioManager.AudioFile.src = "aud/alarm.mp3";
+AudioManager.play = function () { if (AudioManager.AudioLoaded) { AudioManager.AudioFile.play(); }}
+
+function requestNotificationPermission() {
+	Notification.requestPermission(function (permission) { 
+		Notification.permission = permission; 
+		console.log(permission);
+	});
+}
+
+function notifyUser(message) {
+	if ("Notification" in window) {
+		if (!("permission" in Notification) || (Notification.permission === "default")) 
+		{
+			requestNotificationPermission();
+		}
+		
+		if (Notification.permission !== "denied")
+		{
+			var notification = new Notification('Pomodoro do Arara', { body: message, icon: "images/icon-128.png" });
+			window.navigator.vibrate(500);
+		}
+	}
+}
+
+PomodoroEntry = function() { 
+	this.Name = "";
+	this.State = 0;
+	this.Start = ko.observable(null);
+	this.End = ko.observable(null);
+	this.Parent = null;
+	this.FormattedStart = ko.computed(function() { return !!this.Start() ? this.Start().format("MMM DD | HH:mm") : ""; }, this);
+	this.FormattedEnd = ko.computed(function() { return !!this.End() ? this.End().format("MMM DD | HH:mm") : ""; }, this);
+	this.Difference = ko.computed(function() { 
+		if (!!this.Start() && !!this.End()) {
+			var min = this.End().diff(this.Start(), 'minutes');
+			var sec = this.End().diff(this.Start(), 'seconds') % 60;
+			return min + "m " + sec + "s";
+		} else return "";
+	}, this);
+}
+
+PomodoroEntry.Serialize = function(entry) {
+	var a = {};
+	a.Name = entry.Name;
+	a.State = entry.State;
+	a.Start = entry.Start().toJSON();
+	a.End = entry.End().toJSON();
+	return a;
+}
+PomodoroEntry.Deserialize = function(jsonObject, manager) {
+	var a = new PomodoroEntry();
+	a.Name = jsonObject.Name;
+	a.State = jsonObject.State;
+	a.Start(moment(jsonObject.Start));
+	a.End(moment(jsonObject.End));
+	a.Parent = manager;
+	return a;
+}
+
+PomodoroManagerState = {}
+PomodoroManagerState.IDLE = 0;
+PomodoroManagerState.WORKING = 1;
+PomodoroManagerState.SHORTBREAK = 2;
+PomodoroManagerState.LONGBREAK = 3;
+PomodoroManagerState.POTTYPAUSE = 4;
+PomodoroManagerState.getStateName = function (state) {
+	if (state == PomodoroManagerState.IDLE) {
+		return "Idle";
+	} else if (state == PomodoroManagerState.WORKING) {
+		return "Working";
+	} else if (state == PomodoroManagerState.SHORTBREAK) {
+		return "Short Break";
+	} else if (state == PomodoroManagerState.LONGBREAK) {
+		return "Long Break";
+	} else if (state == PomodoroManagerState.POTTYPAUSE) {
+		return "Potty Pause";
+	}
+}
+
+function PomodoroManager() {
+	this.CurrentEntry = null;
+	this.ShortBreakCount = 0;
+	this.IntervalID = null;
+	this.State = ko.observable(PomodoroManagerState.IDLE);
+	this.Entries = ko.observableArray();
+	this.CurrentTaskName = ko.observable("Task");
+	this.Timer = ko.observable(0);
+	this.FormattedTimer = null;
+	this.WorkTime = ko.observable(25);
+	this.ShortBreakTime = ko.observable(5);
+	this.MaxShortBreaks = ko.observable(3);
+	this.LongBreakTime = ko.observable(15);
+	this.StateBeforePause = null;
+	
+	this.FormattedTimer = ko.computed(function() { return this.getFormattedTimer(); }, this);
+	
+	this.configureLocalStorage();
+}
+
+
+PomodoroManager.prototype.startTimer = function() {
+	this.promptForNewTask();
+	this.State(PomodoroManagerState.WORKING);
+	this.resetTimer();
+	this.startEntry();
+	requestNotificationPermission();
+	
+	this.IntervalID = setInterval(this.loop(this), 1000);
+}
+PomodoroManager.prototype.changeTask = function() {
+	this.promptForNewTask();
+	this.endEntry();
+	this.startEntry();
+}
+PomodoroManager.prototype.stopTimer = function() {	
+	this.endEntry();
+	this.State(PomodoroManagerState.IDLE);
+	this.resetTimer();
+	clearInterval(this.IntervalID);
+}
+PomodoroManager.prototype.pottyPause = function() {
+	if (this.State() == PomodoroManagerState.POTTYPAUSE)
+	{
+		this.endEntry();
+		this.State(PomodoroManagerState.WORKING);
+		this.IntervalID = setInterval(this.loop(this), 1000);
+		this.startEntry();
+	}
+	else if (this.State() == PomodoroManagerState.WORKING)
+	{
+		this.endEntry();
+		clearInterval(this.IntervalID);
+		this.State(PomodoroManagerState.POTTYPAUSE);
+		this.startEntry();
+	}
+}
+PomodoroManager.prototype.loop = function(manager) {
+	return function() {
+		manager.Timer(manager.Timer() - 1);
+		if (manager.Timer() == 0)
+		{
+			var oldState = manager.State();
+			AudioManager.play();
+			manager.endEntry();
+			manager.goToNextState();
+			var newState = manager.State();
+			notifyUser("Your " + PomodoroManagerState.getStateName(oldState) + " Pomodoro is over. Starting a " + PomodoroManagerState.getStateName(newState) + " Pomodoro now!");
+			manager.startEntry();
+		}
+	}
+}
+PomodoroManager.prototype.goToNextState = function() {
+	if (this.State() == PomodoroManagerState.IDLE) {
+		return;
+	} else if (this.State() == PomodoroManagerState.SHORTBREAK) {
+		this.ShortBreakCount = this.ShortBreakCount + 1;
+		this.State(PomodoroManagerState.WORKING);
+		
+		this.resetTimer();
+	} else if (this.State() == PomodoroManagerState.LONGBREAK) {
+		this.ShortBreakCount = 0;
+		this.State(PomodoroManagerState.WORKING);
+		this.resetTimer();
+	} else {
+		if (this.ShortBreakCount == this.MaxShortBreaks()) {
+			this.State(PomodoroManagerState.LONGBREAK);
+			this.resetTimer();
+		} else {
+			this.State(PomodoroManagerState.SHORTBREAK);
+			this.resetTimer();
+		}
+	}
+}
+PomodoroManager.prototype.clearAllEntries = function() {
+	this.Entries.removeAll();
+}
+PomodoroManager.prototype.promptForNewTask = function() {
+	var taskname = window.prompt("What are you going to do now?", "New task");
+	this.CurrentTaskName(!!taskname ? taskname : "New task");
+}
+PomodoroManager.prototype.startEntry = function() {
+	var newEntry = new PomodoroEntry();
+	newEntry.Name = !!this.CurrentTaskName() ? this.CurrentTaskName() : "Unnamed Task";
+	newEntry.State = this.State();
+	newEntry.Parent = this;
+	newEntry.Start(moment());
+	this.CurrentEntry = newEntry;
+}
+PomodoroManager.prototype.endEntry = function() {
+	this.CurrentEntry.End(moment());
+	this.Entries.unshift(this.CurrentEntry);
+	this.CurrentEntry = null;
+}
+PomodoroManager.prototype.resetTimer = function() {
+	if (this.State() == PomodoroManagerState.WORKING) {
+		this.Timer(this.WorkTime() * 60);
+	} else if (this.State() == PomodoroManagerState.SHORTBREAK) {
+		this.Timer(this.ShortBreakTime() * 60);
+	} else if (this.State() == PomodoroManagerState.LONGBREAK) {
+		this.Timer(this.LongBreakTime() * 60);
+	} else if (this.State() == PomodoroManagerState.IDLE) {
+		this.Timer(0);
+	}
+}
+PomodoroManager.prototype.getFormattedTimer = function () {
+	if (!this.Timer()) return "00:00";
+	
+	var div = Math.floor(this.Timer()/60);
+	var rem = this.Timer() % 60;
+	div = div < 10 ? "0" + div : div;
+	rem = rem < 10 ? "0" + rem : rem;
+	return div + ":" + rem;
+}
+PomodoroManager.prototype.configureLocalStorage = function () {
+	//HTML5 Local Web Storage support
+	if (typeof(Storage)!=="undefined") {
+		this.updateVersion();
+		
+		if (typeof(localStorage.WorkTime)!=="undefined") this.WorkTime(localStorage.WorkTime * 1);
+		if (typeof(localStorage.ShortBreakTime)!=="undefined") this.ShortBreakTime(localStorage.ShortBreakTime * 1);
+		if (typeof(localStorage.MaxShortBreaks)!=="undefined") this.MaxShortBreaks(localStorage.MaxShortBreaks * 1);
+		if (typeof(localStorage.LongBreakTime)!=="undefined") this.LongBreakTime(localStorage.LongBreakTime * 1);
+		if (typeof(localStorage.Entries)!=="undefined") {
+			var parsedEntries = JSON.parse(localStorage.Entries);
+			var deserializedEntries = parsedEntries.map(function (element) { return PomodoroEntry.Deserialize(element, this); }, this);
+			this.Entries(deserializedEntries);
+		}
+		
+		this.WorkTime.subscribe(function(newValue) { localStorage.WorkTime = newValue; });
+		this.ShortBreakTime.subscribe(function(newValue) { localStorage.ShortBreakTime = newValue; });
+		this.MaxShortBreaks.subscribe(function(newValue) { localStorage.MaxShortBreaks = newValue; });
+		this.LongBreakTime.subscribe(function(newValue) { localStorage.LongBreakTime = newValue; });
+		this.Entries.subscribe(function(newValue) { 
+			var serializedEntries = newValue.map(PomodoroEntry.Serialize);
+			var stringifiedEntries = JSON.stringify(serializedEntries);
+			localStorage.Entries = stringifiedEntries; 
+		});
+	}
+}
+PomodoroManager.prototype.clearEntry = function (entry) {
+	this.Parent.Entries.remove(entry);
+}
+PomodoroManager.prototype.updateVersion = function () {
+	if (typeof(localStorage.Version)=="undefined") {
+		console.log("Updating to version 1.0...");
+		localStorage.Version = "1.0";
+		localStorage.removeItem("Entries");
+	}
+}
